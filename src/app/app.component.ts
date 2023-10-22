51b1204b9c2d90c93eb7c2d98856b9e2de22c635
Singh <Shubham.Singh1@ellucian.com>
2023-10-22 23:14:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import textJson from '../assets/textJson.json';
+
+interface Text {
+  id: string;
+  text1: string;
+  text2: string; 
+  children: Text[]; 
+  expanded?: boolean,
+  selected?: boolean;
+}
+interface Data {
+  rows: Text[];
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  textData: Data = { rows: [] };
+  selectedRows: Text[] = [];
+
+
+  constructor() {
+    this.textData = textJson;
+    this.initializeExpansionState(textJson.rows);
+    console.log(textJson)
+  }
+
+  toggleChildren(row: Text) {
+    row.expanded = !row.expanded;
+  }
+
+  toggleSelection(row: Text) {
+    row.selected = !row.selected;
+    this.toggleChildrenSelection(row, row.selected);
+  }
+
+
+  private toggleChildrenSelection(parent: Text, isSelected: boolean) {
+    if (parent.children) {
+      parent.children.forEach((child) => {
+        child.selected = isSelected;
+        this.toggleChildrenSelection(child, isSelected);
+      });
+    }
+  }
+
+  private initializeExpansionState(data: Text[]) {
+    data.forEach((row) => {
+      row.expanded = false;
+      row.selected = false;
+      if (row.children && row.children.length > 0) {
+        this.initializeExpansionState(row.children);
+      }
+    });
+  }
 }
